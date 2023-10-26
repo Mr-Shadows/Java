054dc317020a23cf446cc4b723b291449a40a4b7
Oleg <111567505+Mr-Shadows@users.noreply.github.com>
2023-10-26 21:19:21 +0300

    fix bugs in task5

--- task5/src/main.java
+++ task5/src/main.java
@@ -1,25 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
-public class main
-{
-    public static void main(String[] args)
-    {
-        readAndPrintOddNumbers();
+public class main {
+    public static void main(String[] args) {
+        List<Integer> numbers = new ArrayList<>();
+        readNumbers(numbers);
+        printOddNumbers(numbers);
     }
 
-    public static void readAndPrintOddNumbers()
-    {
+    public static void readNumbers(List<Integer> numbers) {
         Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+        String[] numbersArray = input.split(" ");
 
-        int number;
-        do
-        {
-            number = scanner.nextInt();
+        for (String numberString : numbersArray) {
+            int number = Integer.parseInt(numberString);
+
+            if (number == 0) {
+                return;
+            }
+
+            numbers.add(number);
+        }
+
+        readNumbers(numbers);
+    }
+
+    public static void printOddNumbers(List<Integer> numbers) {
+        for (int number : numbers) {
             if (number % 2 != 0) {
                 System.out.println(number);
             }
-        } while (number != 0);
-
-        scanner.close();
+        }
     }
 }
